c3ef5caeb53f18929f8c6cd954659e8ef49869b8
XQ-Gang <3035686788@qq.com>
2022-07-02 00:43:48 +0800

    feat: add Append & Pop for List

--- list.go
+++ list.go
@@ -6,3 +6,17 @@ func List(elems ...any) list {
 	l := list(elems)
 	return l
 }
+
+func (l *list) Append(elem any) {
+	*l = append(*l, elem)
+}
+
+func (l *list) Pop() any {
+	size := len(*l)
+	if size == 0 {
+		panic("pop from empty list")
+	}
+	last := (*l)[size-1]
+	*l = (*l)[:size-1]
+	return last
+}
--- list_test.go
+++ list_test.go
@@ -1,6 +1,7 @@
 package collections
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -20,11 +21,47 @@ func TestList(t *testing.T) {
 	}
 }
 
-func TestListSize(t *testing.T) {
+func TestList_Size(t *testing.T) {
 	list := List(1, 3.14, "2", [...]int{1, 2})
 	got := len(list)
 	want := 4
 	if got != want {
 		t.Errorf("len(list) = %v, want %v", got, want)
 	}
+}
+
+func TestList_String(t *testing.T) {
+	list := List(1, 3.14, "2", [...]int{1, 2})
+	got := fmt.Sprintf("%v", list)
+	want := "[1 3.14 2 [1 2]]"
+	if got != want {
+		t.Errorf("%%v = %v, want %v", got, want)
+	}
+}
+
+func TestList_Append(t *testing.T) {
+	list := List(1, 3.14, "2", [...]int{1, 2})
+	list.Append(5)
+	got := list[len(list)-1]
+	want := 5
+	if got != want {
+		t.Errorf("list.Append(5), list[-1] = %v, want %v", got, want)
+	}
+}
+
+func TestList_Pop(t *testing.T) {
+	list := List(1, 3.14, "2", [...]int{1, 2})
+	got := list.Pop()
+	want := [2]int{1, 2}
+	if got != want {
+		t.Errorf("list.Pop() = %v, want %v", got, want)
+	}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("elist.Pop() = nil, want panic(\"pop from empty list\")")
+		}
+	}()
+	elist := List()
+	elist.Pop()
 }
